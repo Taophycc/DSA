bf0861cecd4628b005a555349927c8416a19db54
Taofeek Kassim <taophyckassim@gmail.com>
2025-07-12 20:08:23 +0100

    LeetCode 1672 - Richest Customer Wealth - Difficulty: EASY

---
+++ LeetCode/Arrays/richestCustomerWealth.ts
@@ -0,0 +1,15 @@
+// Difficulty - EASY
+/*
+function maximumWealth(accounts: number[][]): number {
+    let maxSum = 0
+    for (let i = 0; i < accounts.length; i++) {
+        let sum = 0
+        for (let j = 0; j < accounts[i].length; j++) {
+            sum += accounts[i][j]
+        }
+        maxSum = Math.max(maxSum, sum)
+
+    }
+    return maxSum
+};
+*/
