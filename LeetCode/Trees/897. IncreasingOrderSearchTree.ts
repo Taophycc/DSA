c358d42d930ac2d6ed6fd5ff3d06bd3d0b38d013
Taofeek Kassim <taophyckassim@gmail.com>
2025-07-17 10:32:26 +0100

    LeetCode 897 - Increasing Order Search Tree - Difficulty: EASY

---
+++ LeetCode/Trees/897. IncreasingOrderSearchTree.ts
@@ -0,0 +1,24 @@
+// Difficulty - EASY
+/*
+function increasingBST(root: TreeNode | null): TreeNode | null {
+    const result: number[] = [] 
+
+    function inorder(root){
+        if (root == null) return
+        inorder(root.left)
+        result.push(root.val)
+        inorder(root.right)
+    }
+    inorder(root)
+    console.log(result)
+
+    let newRoot = new TreeNode(result.shift())
+    let curr = newRoot
+
+    while(result.length !== 0){
+        curr.right = new TreeNode(result.shift())
+        curr = curr.right
+    }
+    return newRoot
+};
+*/
