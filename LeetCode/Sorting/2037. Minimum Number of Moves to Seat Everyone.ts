ca39b5dc598e77fa5a94e508f5a06f6cc898a3fc
Taofeek Kassim <taophyckassim@gmail.com>
2025-07-19 11:23:29 +0100

    LeetCode 2037 - Minimum Number of Moves to Seat Everyone - Difficulty: EASY

---
+++ LeetCode/Sorting/2037. Minimum Number of Moves to Seat Everyone.ts
@@ -0,0 +1,13 @@
+// Difficulty - EASY
+/*
+function minMovesToSeat(seats: number[], students: number[]): number {
+    seats.sort((a, b) => a - b);
+    students.sort((a, b) => a - b);
+    let sum = 0
+
+    for(let i = 0; i< students.length; i++){
+        sum += Math.abs(seats[i] - students[i]);
+    }
+    return sum
+};
+*/
