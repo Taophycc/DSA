68d70c520c4a5c5e1605a9a744dbd324530c65e9
Taofeek Kassim <taophyckassim@gmail.com>
2025-07-22 00:09:40 +0100

    LeetCode 2974 - Minimum Number Game - Difficulty: EASY

---
+++ LeetCode/Sorting/2974. Minimum Number Game.ts
@@ -0,0 +1,13 @@
+// Difficulty - EASY
+/*
+function numberGame(nums: number[]): number[] {
+    nums.sort((a,b)=> (b-a))
+    let res = []
+    while(nums.length!==0){
+        const alice = nums.pop()
+        const bob = nums.pop()
+        res.push(bob, alice)
+    }
+    return res
+};
+*/
