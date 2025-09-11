59e9cd03bf3ee51ed62b59d82ed98d6726a1da64
Taofeek Kassim <taophyckassim@gmail.com>
2025-09-11 23:49:35 +0100

    LeetCode 2824 - Count Pairs Whose Sum is Less than Target - Difficulty: EASY

---
+++ LeetCode/BinarySearch/2824. Count Pairs Whose Sum is Less than Target.py
@@ -0,0 +1,17 @@
+#Difficulty - MEDIUM
+
+# class Solution:
+#     def countPairs(self, nums: List[int], target: int) -> int:
+#         nums.sort()
+#         cnt = 0
+#         left = 0
+#         right = len(nums) - 1
+
+#         while left < right:
+#             if nums[left] + nums[right] < target:
+#                 cnt += (right - left)
+#                 left +=1
+#             else:
+#                 right -=1
+            
+#         return cnt    
