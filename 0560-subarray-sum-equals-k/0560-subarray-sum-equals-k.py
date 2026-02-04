d50da84ee0e3fd6381a4c2922afc01d7a47abaff
Taofeek Kassim <taophyckassim@gmail.com>
2026-02-04 04:17:35 +0100

    Time: 51 ms (6.04%), Space: 21.9 MB (49.79%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,23 +1,16 @@
 from collections import Counter
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-    # Initialize with 0:1 to handle subarrays starting from the very beginning
+        # Initialize with 0:1 to handle subarrays starting from the very beginning
         cnt = Counter({0:1})
         ans = 0
         s = 0
 
-        # Formula: CurrentPrefixSum - OldPrefixSum = Target(k)
-        # Therefore: OldPrefixSum = CurrentPrefixSum - k
-        
-        for n in nums:
-            # 1. Update Current Prefix Sum
-            s += n 
-            
-            # 2. Check: Have we seen a prefix sum of (s - k) before?
-            # If yes, it means the subarray between that old prefix and now sums to k.
+        for num in nums:
+            s += num
+
             ans += cnt[s-k]
-            
-            # 3. Record the current prefix sum for future checks
+
             cnt[s] += 1
         
         return ans
