7f0b341d38a2cefa65a6f91699c1528228cd7980
Taofeek Kassim <taophyckassim@gmail.com>
2026-02-26 22:01:15 +0100

    Time: 0 ms (100%), Space: 19.4 MB (21.39%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -1,13 +1,11 @@
 class Solution:
     def numSteps(self, s: str) -> int:
-        # 1101 - 13
-        # if odd didvide by 2
         cnt = 0
         num = int(s, 2)
         while num != 1:
             if num & 1:
                 num += 1
             else:
-                num//=2
+                num >>= 1
             cnt += 1
         return cnt
