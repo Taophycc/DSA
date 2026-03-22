9ab75631a2d8f2c850e0c620fdde9b0f1f7a6969
Taofeek Kassim <taophyckassim@gmail.com>
2026-03-22 02:46:00 +0100

    Time: 1015 ms (40.71%), Space: 156.4 MB (26.1%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.py
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.py
@@ -4,19 +4,18 @@ class Solution:
         cols = len(grid[0])
         cnt = 0
 
-        prefix_sum_x = [[0] * (cols+1) for _ in range(rows+1)]
-        prefix_sum_y = [[0] * (cols+1) for _ in range(rows+1)]
-        
+        prefix_x = [[0] * (cols+1) for _ in range(rows+1)]
+        prefix_y = [[0] * (cols+1) for _ in range(rows+1)]
 
-        for r in range(1, rows+1):
+        for r in range(1,rows+1):
             for c in range(1, cols+1):
-                val_x = 1 if grid[r-1][c-1] == 'X' else 0
-                val_y = 1 if grid[r-1][c-1] == 'Y' else 0
+                char = grid[r-1][c-1]
+                val_x = 1 if char == 'X' else 0
+                val_y = 1 if char == 'Y' else 0
 
-                prefix_sum_x[r][c] = prefix_sum_x[r-1][c] + prefix_sum_x[r][c-1] - prefix_sum_x[r-1][c-1] + val_x
-                prefix_sum_y[r][c] = prefix_sum_y[r-1][c] + prefix_sum_y[r][c-1] - prefix_sum_y[r-1][c-1] + val_y
+                prefix_x[r][c] = prefix_x[r-1][c] + prefix_x[r][c-1] - prefix_x[r-1][c-1] + val_x
+                prefix_y[r][c] = prefix_y[r-1][c] + prefix_y[r][c-1] - prefix_y[r-1][c-1] + val_y
 
-                if prefix_sum_x[r][c] == prefix_sum_y[r][c] and prefix_sum_x[r][c] > 0:
+                if prefix_x[r][c] > 0 and prefix_x[r][c] == prefix_y[r][c]:
                     cnt += 1
         return cnt
-                
