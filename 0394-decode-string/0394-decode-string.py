dd840da1b8477e7fe9f7847dbb44bfe4a93ba517
Taofeek Kassim <taophyckassim@gmail.com>
2026-07-06 09:16:08 +0100

    Time: 0 ms (100%), Space: 19.1 MB (95.38%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,29 +1,20 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        # keep pushing until i get to a "]" -> stack = 3
-        # check the top of that stack and pop the values while creating a substring till we get "[" and then pop it
-        # then top of the stack becomes the digit we then multiply that woth the substring
-        # keep going until you hit a “]“ pop the top till the top becomes "[" -> 
         stack = []
-        ans = ""
-        n = len(s)
 
         for char in s:
             if char != "]":
                 stack.append(char)
             else:
-                substring = ""
-                while stack[-1] != "[":
-                    substring = stack.pop() + substring
-                # pop the "["    
+                char_string = ""
+                while stack and stack[-1] != "[":
+                    char_string = stack.pop() + char_string
                 stack.pop()
-                
-                # deal with numbers. edge case it could be multi digit
-                numstring = ""
-                while stack and stack[-1].isdigit():
-                    numstring = stack.pop() + numstring
-                n = int(numstring)
-            
-                stack.append(n*substring)
 
+                num_string = ""
+                while stack and stack[-1].isdigit():
+                    num_string = stack.pop() + num_string
+                n = int(num_string)
+                stack.append(char_string*n)
+        
         return "".join(stack)
