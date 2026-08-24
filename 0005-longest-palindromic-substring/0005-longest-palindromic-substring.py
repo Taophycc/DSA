e4da6c2d64b1e0dfd321c3b462d9488bd66512c3
Taofeek Kassim <taophyckassim@gmail.com>
2026-08-24 16:35:08 +0100

    Time: 321 ms (41.01%), Space: 19.4 MB (35.44%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -3,21 +3,17 @@ class Solution:
         res = ""
         res_len  = 0
 
+        def expand(l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                nonlocal res_len, res
+                if (r-l+1) > res_len:
+                    res = s[l:r+1]
+                    res_len = r-l+1
+                l -= 1
+                r += 1
+
         for i in range(len(s)):
-            l,r, = i, i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r-l+1) > res_len:
-                    res = s[l:r+1]
-                    res_len = r-l+1
-                l -= 1
-                r += 1
-            
-            l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r-l+1) > res_len:
-                    res = s[l:r+1]
-                    res_len = r-l+1
-                l -= 1
-                r += 1
+            expand(i, i)
+            expand(i, i+1)
             
         return res
