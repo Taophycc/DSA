1617ffb22d8428c32324e1e842b6e1d865e47e4d
Taofeek Kassim <taophyckassim@gmail.com>
2026-08-26 15:20:37 +0100

    Time: 35 ms (46.7%), Space: 21.8 MB (56.53%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,16 +1,16 @@
-from collections import Counter
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # Initialize with 0:1 to handle subarrays starting from the very beginning
-        cnt = Counter({0:1})
-        ans = 0
-        s = 0
+        cnt = 0
+
+        prefix_map = {0:1}
+        curr_sum = 0
 
         for num in nums:
-            s += num
+            curr_sum += num
+            p = curr_sum - k
+            
+            if p in prefix_map:
+                cnt+= prefix_map[p]
 
-            ans += cnt[s-k]
-
-            cnt[s] += 1
-        
-        return ans
+            prefix_map[curr_sum] = prefix_map.get(curr_sum, 0) + 1
+        return cnt
