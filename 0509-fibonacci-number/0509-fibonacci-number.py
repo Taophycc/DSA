d79f19f9d9bd91bdb230b72c80ebfe9dd77e8fd5
Taofeek Kassim <taophyckassim@gmail.com>
2026-09-05 03:29:52 +0100

    Time: 35 ms (97.53%), Space: 19.1 MB (89.02%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,17 +1,14 @@
 class Solution:
     def fib(self, n: int) -> int:
-        cache = {}
+        if n <= 1:
+            return n
+
+        dp = [0] * (n+1)
+
+        dp[0] = 0
+        dp[1] = 1
         
-        def dfs(n):
-            if n <= 1:
-                return n
-            if n in cache:
-                return cache[n]
-            
-            left = dfs(n-1)
-            right = dfs(n-2)
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
 
-            cache[n] = left + right
-            return cache[n]
-
-        return dfs(n)
+        return dp[n]
