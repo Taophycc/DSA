eb812460a7ea4a29b4c31aaa9eb939eff2c4ceff
Taofeek Kassim <taophyckassim@gmail.com>
2026-09-06 01:28:51 +0100

    Time: 0 ms (100%), Space: 19.3 MB (80.33%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -4,11 +4,12 @@ class Solution:
         if n <= 1:
             return 0
 
-        dp = [0]*(n+1)
-        dp[0] = cost[0]
-        dp[1] = cost[1]
+        prev2 = cost[0]
+        prev1 = cost[1]
 
         for i in range(2, n+1):
             curr_cost = cost[i] if i < n else 0
-            dp[i] = curr_cost + min(dp[i-1], dp[i-2])
-        return dp[n]
+            current = curr_cost + min(prev1, prev2)
+            prev2 = prev1
+            prev1 = current
+        return prev1
